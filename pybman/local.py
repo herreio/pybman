d8e3e8040f670b97dff3b4f8b46e649b30a28d4d
herreio <mail@herre.io>
2019-03-05 14:01:03 +0100

    improve local data management

--- pybman/local.py
+++ pybman/local.py
@@ -3,55 +3,72 @@ import pkg_resources
 
 from datetime import date
 
-from pybman.utils import read_plain_clean
-from pybman.utils import write_list
-from pybman.utils import read_json, write_json
+from pybman import data
+from pybman import utils
+
+from pybman.data import DataSet
+
+# from pybman.utils import read_plain_clean
+# from pybman.utils import write_list
+# from pybman.utils import read_json, write_json
 
 class LocalData:
 
-    def __init__(self, base_dir='data/'):
+    def __init__(self, base_dir='./data/', create=False):
 
-        self.base_dir = base_dir
-
-        self.data_dir = self.base_dir + 'pubs/'
-        self.data_paths = []
-        self.data_paths_txt = self.base_dir + 'pubs.txt'
+        self.data_dir = os.path.realpath(base_dir)
+        self.data_file = os.path.join(self.data_dir, 'paths.txt')
         self.data_exists = True
 
-        self.pers_dir = self.base_dir + 'pers/'
-        self.pers_paths = []
-        self.pers_paths_txt = self.base_dir + 'pers.txt'
+        self.ou_dir = os.path.join(self.data_dir, 'ou')
+        self.ctx_dir = os.path.join(self.data_dir, 'ctx')
+        self.pers_dir = os.path.join(self.data_dir, 'pers')
+
+        self.ou_exists = True
+        self.ctx_exists = True
         self.pers_exists = True
 
-        if not os.path.exists(self.base_dir):
-            self.data_exists = False
-            self.pers_exists = False
-            os.mkdir(self.base_dir)
-            os.mkdir(self.data_dir)
-            os.mkdir(self.pers_dir)
+        # self.data_dir = self.base_dir + '/pubs/'
+        # self.data_paths_txt = self.base_dir + 'pubs.txt'
+
+        self.data_paths = []
+
+        # self.pers_paths = []
+        # self.pers_paths_txt = self.base_dir + 'pers.txt'
+        # self.pers_exists = True
 
         if not os.path.exists(self.data_dir):
-            os.mkdir(self.data_dir)
             self.data_exists = False
-
-        if not os.path.exists(self.pers_dir):
-            os.mkdir(self.pers_dir)
+            self.ou_exists = False
+            self.ctx_exists = False
             self.pers_exists = False
+            if create:
+                os.mkdir(self.data_dir)
+                os.mkdir(self.ou_dir)
+                os.mkdir(self.ctx_dir)
+                os.mkdir(self.pers_dir)
+
+        if not os.path.exists(self.ou_dir) and ou_exists:
+            self.ou_exists = False
+            if create:
+                os.mkdir(self.ou_dir)
+
+        if not os.path.exists(self.ctx_dir) and ctx_exists:
+            self.ctx_exists = False
+            if create:
+                os.mkdir(self.ctx_dir)
+
+        if not os.path.exists(self.pers_dir) and pers_exists:
+            self.pers_exists = False
+            if create:
+                os.mkdir(self.pers_dir)
 
         if self.data_exists:
-            if os.path.exists(self.data_paths_txt):
-                self.data_paths = read_plain_clean(self.data_paths_txt)
+            if os.path.exists(self.data_file):
+                self.data_paths = utils.read_plain_clean(self.data_file)
                 if self.data_paths:
                     print('local pulication data:')
                     for p in self.data_paths:
-                        print(p)
-
-        if self.pers_exists:
-            if os.path.exists(self.pers_paths_txt):
-                self.pers_paths = read_plain_clean(self.pers_paths_txt)
-                if self.pers_paths:
-                    print('local person data:')
-                    for p in self.pers_paths:
                         print(p)
 
     # find path by given entity (id)
@@ -69,26 +86,27 @@ class LocalData:
     def get_data(self, data_id):
         path = self.find_data_path(data_id)
         if path:
-            return read_json(path)
+            json_data = utils.read_json(path)
+            return data.DataSet(data_id, data=json_data)
         else:
             print("could not find local data!")
             return {}
+
+    # write local data file
+    def store_data(self, idx, data):
+        print("store local data of", idx)
+        self.change_data_path(idx)
+        path = self.generate_data_path(idx)
+        utils.write_json(path, data)
 
     # get data path for given id
     def generate_data_path(self, data_id):
         return self.data_dir + data_id + "--" + date.today().isoformat() + ".json"
 
-    # remove old local data
-    def clean_local_data(self, idx):
-        path = self.find_data_path(idx)
-        if path:
-            print("removing file", path)
-            os.remove(path)
-        else:
-            print("failed to remove file from entity", idx)
-
-    # update list of paths
     def change_data_path(self, idx):
+        """
+        update list of paths
+        """
         pos = -1
         for i, path in enumerate(self.data_paths):
             if idx in path:
@@ -97,24 +115,30 @@ class LocalData:
         new = self.generate_data_path(idx)
         if pos < 0:
             self.data_paths.append(new)
-            write_list(self.data_paths_txt, self.data_paths)
+            utils.write_list(self.data_file, self.data_paths)
             print("local data file added", new)
         else:
             old = self.data_paths[pos]
             # self.clean_local_data(old)
             self.data_paths[pos] = new
-            write_list(self.data_paths_txt, self.data_paths)
+            utils.write_list(self.data_file, self.data_paths)
             print("local data file updated", new)
 
-    # write local data file
-    def store_data_local(self, idx, data):
-        print("store local data of", idx)
-        self.change_data_path(idx)
-        path = self.generate_data_path(idx)
-        write_json(path, data)
+    # remove old local data
+    def clean_local_data(self, idx):
+        """
+        clean up local data
+        """
+        path = self.find_data_path(idx)
+        if path:
+            print("removing file", path)
+            os.remove(path)
+        else:
+            print("failed to remove file from entity", idx)
 
-    def store_titles_local(self, idx, data):
-        pass
 
-def resolve_path(path):
-    return pkg_resources.resource_filename('pybman', path)
+#    def store_titles_local(self, idx, data):
+#        pass
+
+# def resolve_path(path):
+#    return pkg_resources.resource_filename('pybman', path)
