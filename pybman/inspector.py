bd12e82f9406d3c5e62deb9b4b6c2958d33daa3f
herreio <mail@herre.io>
2019-03-19 12:50:06 +0100

    add title inspection

--- pybman/inspector.py
+++ pybman/inspector.py
@@ -1,16 +1,40 @@
+# from pybman import rest
+
 class Inspector:
 
     def __init__(self, records):
         self.records = records
 
-    def check_publication_titles(self):
+    def check_publication_titles(self, clean=False):
         updates = {}
         for record in self.records:
+            item_id = record['data']['objectId']
             publication_title = record['data']['metadata']['title']
             if publication_title != publication_title.strip():
-                item_id = record['data']['objectId']
+                if clean:
+                    record['data']['metadata']['title'] = publication_title.strip()
                 updates[item_id] = record
         return updates
+
+    def check_source_titles(self, clean=False):
+        updates = {}
+        for record in self.records:
+            item_id = record['data']['objectId']
+            if 'sources' in record['data']['metadata']:
+                for source in record['data']['metadata']['sources']:
+                    if source['title'] != source['title'].strip():
+                        if clean:
+                            source['title'] = source['title'].strip()
+                        updates[item_id] = record
+        return updates
+
+    # @staticmethod
+    # def clean_publication_titles(records):
+    #    for k in records:
+    #        publication = records[k]
+    #        dirty_title = publication['data']['metadata']['title']
+    #        publication['data']['metadata']['title'] = dirty_title.strip()
+    #    return records
 
     def check_publication_date(self):
         # dateAccepted
