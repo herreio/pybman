35afd47144917e287cc466cfeb3563086e0386a4
herreio <mail@herre.io>
2019-03-19 12:50:35 +0100

    add func inspect_titles / update_data

--- pybman/client.py
+++ pybman/client.py
@@ -2,6 +2,7 @@ from pybman import data
 # from pybman import local
 from pybman import query
 from pybman import rest
+from pybman import inspector
 # from pybman import utils
 
 
@@ -24,6 +25,9 @@ class Client:
         self.ou_query = query.OrgUnitQuery()
         self.ctx_query = query.ContextQuery()
 
+        # inspector class
+        self.inspector = None
+
     def get_data(self,ctx_id=None,ou_id=None,pers_id=None,released=False):
         if ctx_id:
             query = self.ctx_query.get_item_query(ctx_id)
@@ -40,5 +44,36 @@ class Client:
         else:
             print("please specify data to retrieve!")
 
-    def inspect_data(self):
-        pass
+    def update_data(self, idx, data, comment):
+        updated_data = self.item_rest.update_item(idx, data)
+        response = self.item_rest.release_item(idx, updated_data, 'auto-update: title stripped')
+        print("successfully updated data with id", response['objectId'])
+
+    def inspect_titles(self, data):
+        print("starting title inspection!")
+        # create inspector instance
+        self.inspector = inspector.Inspector(data.records)
+        ### inspect publication titles ###
+        clean_data = self.inspector.check_publication_titles(clean=True)
+        if clean_data:
+            counter = 0
+            for k in clean_data:
+                self.update_data(k, clean_data[k]['data'], 'auto-update: publication title stripped')
+                counter += 1
+            print("updated", counter, "publication titles!")
+            total += counter
+        else:
+            print("publication title data is already clean! nothing to do...")
+        ### inspect source titles ###
+        print("inspecting source titles:")
+        clean_data = self.inspector.check_source_titles(clean=True)
+        if clean_data:
+            counter = 0
+            for k in clean_data:
+                self.update_data(k, clean_data[k]['data'], 'auto-update: source title stripped')
+                counter += 1
+            print("updated", counter, "source titles!")
+            total += counter
+        else:
+            print("source title data is already clean! nothing to do...")
+        print("updated", total, "items.")
