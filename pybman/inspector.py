3e6393d8ed2bb1d37d06ebe9f019aab476bc644a
herreio <mail@herre.io>
2019-03-19 15:01:02 +0100

    inspect publishers and publishingPlaces

--- pybman/inspector.py
+++ pybman/inspector.py
@@ -8,9 +8,9 @@ class Inspector:
     def check_publication_titles(self, clean=False):
         updates = {}
         for record in self.records:
-            item_id = record['data']['objectId']
             publication_title = record['data']['metadata']['title']
             if publication_title != publication_title.strip():
+                item_id = record['data']['objectId']
                 if clean:
                     record['data']['metadata']['title'] = publication_title.strip()
                 updates[item_id] = record
@@ -19,13 +19,59 @@ class Inspector:
     def check_source_titles(self, clean=False):
         updates = {}
         for record in self.records:
-            item_id = record['data']['objectId']
             if 'sources' in record['data']['metadata']:
                 for source in record['data']['metadata']['sources']:
                     if source['title'] != source['title'].strip():
+                        item_id = record['data']['objectId']
                         if clean:
                             source['title'] = source['title'].strip()
                         updates[item_id] = record
+        return updates
+
+    def check_publishers(self, clean=False):
+        updates = {}
+        for record in self.records:
+            if 'publishingInfo' in record['data']['metadata']:
+                if 'publisher' in record['data']['metadata']['publishingInfo']:
+                    publisher = record['data']['metadata']['publishingInfo']['publisher']
+                    if publisher != publisher.strip():
+                        item_id = record['data']['objectId']
+                        if clean:
+                            record['data']['metadata']['publishingInfo']['publisher'] = publisher.strip()
+                        updates[item_id] = record
+            if 'sources' in record['data']['metadata']:
+                for source in record['data']['metadata']['sources']:
+                    if 'publishingInfo' in source:
+                        if 'publisher' in source['publishingInfo']:
+                            publisher = source['publishingInfo']['publisher']
+                            if publisher != publisher.strip():
+                                item_id = record['data']['objectId']
+                                if clean:
+                                    source['publishingInfo']['publisher'] = publisher.strip()
+                                updates[item_id] = record
+        return updates
+
+    def check_publishing_places(self, clean=False):
+        updates = {}
+        for record in self.records:
+            if 'publishingInfo' in record['data']['metadata']:
+                if 'place' in record['data']['metadata']['publishingInfo']:
+                    place = record['data']['metadata']['publishingInfo']['place']
+                    if place != place.strip():
+                        item_id = record['data']['objectId']
+                        if clean:
+                            record['data']['metadata']['publishingInfo']['place'] = place.strip()
+                        updates[item_id] = record
+            if 'sources' in record['data']['metadata']:
+                for source in record['data']['metadata']['sources']:
+                    if 'publishingInfo' in source:
+                        if 'place' in source['publishingInfo']:
+                            place = source['publishingInfo']['place']
+                            if place != place.strip():
+                                item_id = record['data']['objectId']
+                                if clean:
+                                    source['publishingInfo']['place'] = place.strip()
+                                updates[item_id] = record
         return updates
 
     # @staticmethod
