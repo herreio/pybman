2a9a1e0c32dd976452e215e1b66ec2d2a4df1c3d
herreio <mail@herre.io>
2019-04-16 15:26:52 +0200

    split cleaning of titles and source titles, create new inspector every time a cleaning process starts

--- pybman/client.py
+++ pybman/client.py
@@ -65,59 +65,67 @@ class Client:
 
         print("start cleaning title data!")
 
-        # create inspector instance if necessary
-        if not self.inspector:
-            if data_set:
-                self.clean_init(data_set)
-            else:
-                print("failed to initialize inspector!")
-                print("please pass data to be cleaned.")
-                return 0
+        # create new inspector instance
+        if data_set:
+            self.clean_init(data_set)
+        else:
+            print("failed to initialize inspector!")
+            print("please pass data to be cleaned.")
+            return 0
 
         # cleaning of publication titles
         clean_data = self.inspector.check_publication_titles(clean=True)
         total = 0
         if clean_data:
-            counter = 0
             for k in clean_data:
                 updated = self.update_data(k, clean_data[k]['data'], 'auto-update: publication title stripped')
                 if updated:
-                    counter += 1
-            print("updated", counter, "publication titles!")
-            total += counter
+                    total += 1
+            print("updated", total, "publication titles!")
         else:
             print("publication title data is already clean!")
             print("nothing to do...")
 
+        return total
+
+    def clean_source_titles(self, data_set=None):
+
+        print("start cleaning source title data!")
+
+        # create new inspector instance
+        if data_set:
+            self.clean_init(data_set)
+        else:
+            print("failed to initialize inspector!")
+            print("please pass data to be cleaned.")
+            return 0
+
         # cleaning of source title data
         clean_data = self.inspector.check_source_titles(clean=True)
+        total = 0
         if clean_data:
-            counter = 0
             for k in clean_data:
                 updated = self.update_data(k, clean_data[k]['data'], 'auto-update: source title stripped')
                 if updated:
-                    counter += 1
-            print("updated", counter, "source titles!")
-            total += counter
+                    total += 1
+            print("updated", total, "source titles!")
         else:
             print("source title data is already clean!")
             print("nothing to do...")
 
-        print("updated", total, "items.")
         return total
 
     def clean_publishers(self, data_set=None):
 
         print("start cleaning publisher data!")
 
-        # create inspector instance if necessary
-        if not self.inspector:
-            if data_set:
-                self.clean_init(data_set)
-            else:
-                print("failed to initialize inspector!")
-                print("please pass data to be cleaned.")
-                return 0
+        # create new inspector instance
+        if data_set:
+            self.clean_init(data_set)
+        else:
+            print("failed to initialize inspector!")
+            print("please pass data to be cleaned.")
+            return 0
 
         # cleaning of publisher data
         clean_data = self.inspector.check_publishers(clean=True)
@@ -138,14 +146,13 @@ class Client:
 
         print("start cleaning publishing place data!")
 
-        # create inspector instance if necessary
-        if not self.inspector:
-            if data_set:
-                self.clean_init(data_set)
-            else:
-                print("failed to initialize inspector!")
-                print("please pass data to be cleaned.")
-                return 0
+        # create new inspector instance
+        if data_set:
+            self.clean_init(data_set)
+        else:
+            print("failed to initialize inspector!")
+            print("please pass data to be cleaned.")
+            return 0
 
         # start cleaning of publishing place data
         clean_data = self.inspector.check_publishing_places(clean=True)
