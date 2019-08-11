7b72c206edadf40bdfe396156b2e4e9adc5de045
herreio <mail@herre.io>
2019-08-11 11:44:52 +0200

    version updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Donatus Herre'
 author = 'Donatus Herre'
 
 # The short X.Y version
-version = '2019.8.2'
+version = '2019.8.11'
 # The full version, including alpha/beta/rc tags
-release = '2019.8.2'
+release = '2019.8.11'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="pybman",
-    version="2019.8.2",
+    version="2019.8.11",
     author="Donatus Herre",
     author_email="pypi@herre.io",
     license="GPL",
