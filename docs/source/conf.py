7e3d1ac10f7d8e03ef6552208787d9f6bd0252b8
herreio <mail@herre.io>
2019-08-11 23:10:37 +0200

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Donatus Herre'
 author = 'Donatus Herre'
 
 # The short X.Y version
-version = '2019.8.11'
+version = '2019.8.12'
 # The full version, including alpha/beta/rc tags
-release = '2019.8.11'
+release = '2019.8.12'
 
 
 # -- General configuration ---------------------------------------------------
